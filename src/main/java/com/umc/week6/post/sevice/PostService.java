d56be6f7ee6160358b5f3fad4f488f395d354aa6
yeonhee <ydol2@naver.com>
2022-11-06 23:43:01 +0900

    feat : 게시판 전체조회 로직 구현

--- src/main/java/com/umc/week6/post/sevice/PostService.java
+++ src/main/java/com/umc/week6/post/sevice/PostService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.umc.week6.exception.ErrorCode.POST_NOT_FOUND;
@@ -49,5 +50,10 @@ public class PostService {
         Post post = checkPost.orElseThrow(() ->
                 new NotFoundException(POST_NOT_FOUND));
        return new PostDetailDto(post);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Post> getPost() {
+        return postRepository.findAll();
     }
 }
